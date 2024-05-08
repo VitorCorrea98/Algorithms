3ada5d9d4c06c6ad7c19cc46ee373ab727524d7a
Vitor Correa <vrcnode@gmail.com>
2024-05-08 19:33:24 -0300

    Requisito 1 feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or 0:
+        return None
+
+    counter = 0
+    for schedule in permanence_period:
+        if not all(
+            isinstance(schedule_time, int) for schedule_time in schedule
+        ):
+            return None
+        if schedule[0] <= target_time <= schedule[1]:
+            counter += 1
+    return counter
