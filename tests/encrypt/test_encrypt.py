5719e235bb989f609f340d8dc1bdbf8bbf1b0401
Vitor Correa <vrcnode@gmail.com>
2024-05-08 20:14:10 -0300

    Requisito 2 feito

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("fluzao", 5) == "azulf_o"
+    assert encrypt_message("fluzao", 2) == "oazu_lf"
+    assert encrypt_message("fluzao", 10) == "oazulf"
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(2, 3) == "ulf_oaz"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("fluzao", "flu") == "ulf_oaz"
