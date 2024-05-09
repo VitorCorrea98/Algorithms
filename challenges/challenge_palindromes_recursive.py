09c4c3d05d6de4af6eddcc1d05dd1e7273c52cc0
Vitor Correa <vrcnode@gmail.com>
2024-05-09 13:07:34 -0300

    Requisito 3 feito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not bool(word):
+        return False
+    low_word = word[low_index]
+    high_word = word[high_index]
+    if low_index >= high_index:
+        return True
+    if low_word == high_word:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    elif low_word != high_word:
+        return False
